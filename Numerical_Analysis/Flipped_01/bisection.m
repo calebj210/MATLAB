0266a8a4631b85bbd911b048c5a5f90b67ab5b94
Caleb Jacobs <calebjacobs210@gmail.com>
2021-09-02 22:47:05 -0600

    Worked on HW1 of numerical analysis

--- Numerical_Analysis/Flipped_01/bisection.m
+++ Numerical_Analysis/Flipped_01/bisection.m
@@ -1,41 +1,48 @@
 % Bisection routine for computing roots of functions
 % Author: Caleb Jacobs
-% Date last modified: 26-08-2021
+% Date last modified: 02-09-2021
 
 format longE
 
-% Functions to find the roots of
-f1 = @(x) (x - 1) * (x - 3) * (x - 5);
-f2 = @(x) (x - 1)^2 * (x - 3);
-f3 = @(x) sin(x);
+% Function defintions
+f1 = @(x) (x - 5).^9;
+f2 = @(x) -1953125 + 3515625*x - 2812500*x.^2 + 1312500*x.^3 - ...
+          393750*x.^4 + 78750*x.^5 - 10500*x.^6 + ...
+          900*x.^7 - 45*x.^8 + x.^9;
 
-% Compute the roots of the functions above
-bisect(0, 2.4, f1, 10^(-5), 100)        % f1 root finding
+% Set parameters
+a   = 4.8;
+b   = 5.31;
+tol = 1e-4;
+maxIts = 20;
 
-bisect(0, 2, f2, 10^(-5), 100)          % f2 root finding
+% Find root with bisection
+bisect(a, b, f1, tol, maxIts);
+bisect(a, b, f2, tol, maxIts);
 
-bisect(0, 0.1, f3, 10^(-5), 100)        % f3 root finding
-bisect(0.5, 3*pi/4, f3, 10^(-5), 100)   % f3 root finding
-
-
-% Bisection algorithm definition
+% Bisection method definition
 function c = bisect(a, b, f, tol,  maxIts)
-    i = 1;
+    % Check if root is guaranteed in the interval
+    if f(a) * f(b) > 0
+        fprintf('Root not guarnteed, exiting!\n\n')
+        return
+    end
     
-    while i <= maxIts
+    for i = 1:maxIts
         c = (b + a) / 2;                    % Midpoint of interval
         
-        if f(c) == 0 || (b - a) / 2 < tol   % Check convergence criteria
-            i                               % Display number of iterations
-            break;
+        if f(c) == 0 || (b - c) < tol       % Check convergence criteria
+            fprintf('Its used = %d\n',i)    % Display number of iterations
+            fprintf('x = %.16f\n\n', c)     % Display root information
+            return
         end
         
-        i = i + 1;
-        
-        if sign(f(c)) == sign(f(a))         % Update searching interval
+        if f(c) * f(a) >= 0                 % Update searching interval
             a = c;
         else
             b = c;
         end
     end
+    
+    fprintf('Convergence could not happen to desired tolerance\n\n')
 end
--- Numerical_Analysis/HW01/Problem_3.m
+++ Numerical_Analysis/HW01/Problem_3.m
@@ -5,13 +5,46 @@
 % Function defintions
 f1 = @(x, delta) cos(x + delta) - cos(x);
 f2 = @(x, delta) -2 * sin((2*x + delta) / 2) .* sin(delta / 2);
-f3 = @(x, delta, xi) -delta * sin(x) - (delta .^ 2) / 2 * cos(xi);
+f3 = @(x, delta) -delta * sin(x) - (delta .^ 2) / 2 * cos(x);
 
 % Variable defintions
-X = 0;
-Deltas = logspace(-16, 0);
+X = logspace(-16,10,10000);
+% X = linspace(0, 10000, 1000);
+startColor = [0.5, 0.5, 0.5];
+D = logspace(-16, 0, 17);
+
+close all
 
 % Plot differences
-loglog(Deltas, abs(f1(X, Deltas) - f2(X, Deltas)), 'LineWidth', 1.5)
+figure
+for i = 1:length(D)
+    loglog(X, abs(f1(X, D(i)) - f2(X, D(i))), 'LineWidth', 1.5, 'Color', startColor + (i - 1) / 32)
+    hold on
+end
+title('Original vs My Expression')
+ylim([1e-40 1e0])
+xlabel('x')
+ylabel('Abs error')
+hold off
 
-% loglog(Deltas, abs(f1(X, Deltas) - f3(X, Deltas, X)), 'LineWidth', 1.5)
+figure
+for i = 1:length(D)
+    loglog(X, abs(f3(X, D(i)) - f2(X, D(i))), 'LineWidth', 1.5, 'Color', startColor + (i - 1) / 32)
+    hold on
+end
+title('My Expression vs Taylor')
+ylim([1e-40 1e0])
+xlabel('x')
+ylabel('Abs error')
+hold off
+
+figure
+for i = 1:length(D)
+    loglog(X, abs(f3(X, D(i)) - f1(X, D(i))), 'LineWidth', 1.5, 'Color', startColor + (i - 1) / 32)
+    hold on
+end
+title('Original vs Taylor')
+ylim([1e-40 1e0])
+xlabel('x')
+ylabel('Abs error')
+hold off
